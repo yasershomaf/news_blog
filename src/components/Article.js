f86482b10e01e4212099ed676746928b32a5ed84
Yaser Somaf <yasersomaf@192.168.2.45>
2019-06-20 21:44:26 +0200

    Created Articl page

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import '../styles/article.css';
 
 export default class Article extends Component {
-  render() {
-    console.log(this.props)
-    return (
-      <div>
-        
-      </div>
-    )
+  componentDidMount() {
+    if (this.props.location.state.article) {
+      this.setState(this.props.location.state.article);
+    }
+    else {
+
+    }
   }
-}
+  render() {
+    const article = this.state;
+    return <Fragment>
+      {article ? <Fragment>
+        {article.title ? <h1 className='article_title'>{article.title}</h1> : ''}
+        {article.description ? <h2 className='article_description'>{article.description}</h2> : ''}
+        {article.urlToImage ? <img
+          className='article_image'
+          alt='Article pic'
+          src={article.urlToImage}
+        /> : ''}
+        <div className='article_author'>
+          {article.author ? `Author: ${article.author} - ` : ''}
+          {article.source && article.source.name ? `Source: ${article.source.name} - ` : ''}
+          {article.publishedAt ? `Published at: ${
+            new Date(article.publishedAt).toDateString()
+          }` : ''}
+        </div>
+        {article.content ? <p>{article.content} {
+          article.url ? <a href={article.url} target='_blank'>More info</a> : ''
+        }</p> : ''}
+        
+      </Fragment> : ''}
+    </Fragment>;
+  }
+};
--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -15,10 +15,11 @@ export default function ArticleCard({article, searchQueries, index}) {
       article: article
     }
   }} className='article_card'>
-    {article.urlToImage ? <img className='card_image' src={article.urlToImage}/> : ''}
+    {article.urlToImage ? <img alt='Article pic' className='card_image' src={article.urlToImage}/> : ''}
     {article.title ? <h1 className='card_title'>{article.title}</h1> : ''}
     <div className='card_author'>
       {article.author ? `${article.author} - ` : ''}
+      {article.source && article.source.name ? `Source: ${article.source.name} - ` : ''}
       {article.publishedAt ? new Date(article.publishedAt).toDateString() : ''}
     </div>
     {article.description ? <p className='card_description'>{article.description}</p> : ''}
